b38582413198903353e4c036a8222d6a7ffb1742
ChrisCPO <ChrisCPO@gmail.com>
2015-03-24 17:35:44 -0400

    added root constraints to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
+class SignedIn
+  def matches?(request)
+    request.session['user_id'].present?
+  end
+end
+
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
-  resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
+
+  constraints(SignedIn.new) do
+    root "dashboards#show", as: :dashboard
+  end
 
   root "users#new"
 end
