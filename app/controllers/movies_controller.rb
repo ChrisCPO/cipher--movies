9a379870e26b0cb1bb94cfb26b9a0ce83e7ff9d4
Quincy Iheme <qiheme@gmail.com>
2015-06-03 11:05:23 -0400

    logic for sending emails if subscribed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,41 +1,71 @@
 class MoviesController < ApplicationController
+  helper_method :subscribe, :notify
+
   def index
     @movies = Movie.all
   end
 
   def new
-    # If there isn't a searched movie in memory already, find one
     if params[:movie] != nil
       @movie = find_movie(params[:movie])
-
-      # if you have already searched for this movie and it is not avialable, ask to subscribe to an email notification
-      if Movie.where(name: @movie, user_id: current_user.id, subscribed: false, notified: false).name == @movie
-        Movie.where(name: @movie, user_id: current_user.id, subscribed: false, notified: false).subscribed = true
-        subscribe(@movie)
-      elsif Movie.where(name: @movie, user_id: current_user.id, subscribed: true, notified: false).name == @movie
-        Movie.where(name: @movie, user_id: current_user.id, subscribed: true, notified: false).notified = true
-        notify(@movie)
-      end
     end
+    puts @movie
+    puts @movie_status
   end
 
   def create
+    binding.pry
     movie = Movie.new(name: @movie, user_id: current_user.id)
-    movie.save
-    redirect_to :movies
+    if @movie != nil && @movie_status == false
+      movie.save
+      subscribe(@movie)
+      redirect_to :movies
+    elsif @movie != nil
+      movie.update(subscribed: true, notified: true)
+      movie.save
+      redirect_to :movies
+    end
   end
 
-  def find_movie(name)
-    JSON.parse(
-      HTTParty.get("https://itunes.apple.com/search?entity=movie&term=#{name}").body
-      )["results"].first["trackName"]
-  end
+  private
 
-  def subscribe(movie)
-    ModelMailer.new_record_notification(current_user.name, current_user.email, movie).deliver
-  end
+    def find_movie(name)
+      response =  JSON.parse(HTTParty.get("https://itunes.apple.com/search?entity=movie&term=#{name}").body)
 
-  def notify(movie)
-    ModelMailer.movie_available_email(current_user.name, current_user.email, movie).deliver
-  end
+      if response["resultCount"] == 0
+        @movie_status = false
+        return name
+      else
+        response["results"].first["trackName"]
+      end
+    end
+
+    def movie_search(movie)
+      Movie.where(name: movie).each do |movie|
+        response =  JSON.parse(HTTParty.get("https://itunes.apple.com/search?entity=movie&term=#{movie.name}").body)
+        if response["resultCount"] != 0
+          notify(movie.name)
+        end
+      end
+    end
+
+    def subscribe(movie)
+      movies = Movie.where(name: movie, user_id: current_user.id, subscribed: false, notified: false)
+      movies.each do |movie|
+        movie.update(subscribed: true)
+      end
+      ModelMailer.new_record_notification(current_user.name, current_user.email, movie).deliver
+    end
+
+    def notify(movie)
+      movies = Movie.where(name: movie, user_id: current_user.id, subscribed: true, notified: false)
+      movies.each do |movie|
+        movie.update(notified: true)
+      end
+      ModelMailer.movie_available_email(current_user.name, current_user.email, movie).deliver
+    end
+
+    def movie_params
+      params.require(:movie).permit(:subscribed, :notified)
+    end
 end
